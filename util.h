c0e5760e1bb55d34da09c48d036591916a898af2
ZenMoore <1057398161@qq.com>
2020-06-24 18:14:17 +0800

    launch.

---
+++ util.h
@@ -0,0 +1,23 @@
+//
+// Created by ZenMoore on 2020/6/24.
+//
+
+#ifndef ZST_DME_UTIL_H
+#define ZST_DME_UTIL_H
+
+struct TreeNode{
+    float x;
+    float y;
+    TreeNode father;
+    TreeNode left_child;
+    TreeNode right_child;
+};
+struct TreeNode tree;// 这个是根节点
+
+void insert_left(struct TreeNode root, float x, float y){
+    struct TreeNode left;
+    
+}
+
+
+#endif //ZST_DME_UTIL_H
